1a2ce9b202af977bb51e5a98ced4b73c7b914abe
vincentt14 <vincent14@student.umn.ac.id>
2023-01-22 01:05:25 +0700

    Create Zoo.js

---
+++ Test-Code-HackerEarth/Zoo.js
@@ -0,0 +1,29 @@
+const x = "o";
+const y = "z";
+
+// y === 2x
+
+const test1 = "zzoooo";
+const test2 = "zzzoooo";
+const test3 = "zzzoooooo";
+
+let jumlahZ = 0;
+let jumlahO = 0;
+
+const pengecekHandal = (testCase) => {
+  jumlahZ = jumlahO = 0;
+  for (let i = testCase.length - 1; i >= 0; i--) {
+    if (testCase[i] === "z") {
+      jumlahZ++;
+    } else jumlahO++;
+  }
+
+  if (jumlahZ * 2 === jumlahO) {
+    return true;
+  }
+  return false;
+};
+
+console.log(pengecekHandal(test1));
+console.log(pengecekHandal(test2));
+console.log(pengecekHandal(test3));
