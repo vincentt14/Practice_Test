13133895f4248b74f5f72adda738da945c3824df
vincentt14 <vincent14@student.umn.ac.id>
2023-01-21 23:39:16 +0700

    Create Soal01.js

---
+++ Test-Code-04/Soal01.js
@@ -0,0 +1,15 @@
+/*
+  given a string 
+  reverse it
+  reverse('apple')=== 'elppa';
+  reverse('Greetings!')= '!sgniteerG';
+*/
+
+const buah = "apple";
+let hasil = "";
+
+for (let i = buah.length - 1; i >= 0; i--) {
+  hasil += buah[i];
+}
+console.log(hasil);
+
