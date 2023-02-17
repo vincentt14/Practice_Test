c2bb7c9607bb56730fae4efb8fa1a40ddae0be78
Vincent <vincent14@student.umn.ac.id>
2023-02-18 01:37:20 +0700

    Update Soal06.js

--- Test-Code-04/Soal06.js
+++ Test-Code-04/Soal06.js
@@ -1,6 +1,9 @@
 const arr1 = [1, 2, 3, 4, 5, 1, 1, 2, 4];
 // n = 2
 
-const pengeChunkHandal = (array) => {
-  
+const chunkSize = 2;
+
+for (let i = 0; i < arr1.length; i += chunkSize) {
+  const chunk = arr1.slice(i, i + chunkSize);
+  console.log(chunk);
 }
