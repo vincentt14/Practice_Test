e9a7615fab3b177513bb7a955ca255404f361bfa
Vincent <vincent14@student.umn.ac.id>
2023-08-16 00:31:47 +0700

    virus spreds solved

--- Competitive-Programming/build/virus-spreads.js
+++ Competitive-Programming/build/virus-spreads.js
@@ -1,43 +1,46 @@
 // virus can infect something that colose to it.
 // we want to simulate the spreads of the virus by its hours infected
 // notes:
-// Code          Condition
-// o             Normal
-// *             Infected
-// #             Immuned
+// Code     Condition
+// o        Normal
+// *        Infected
+// #        Immuned
 // rules:
 // - first line of input is the current conditions of the people
 // - second line of input is the hours of the virus spreads
 // - the output is the final condition of the people after hours they infected
-// - every next hours:  
+// - every next hours:
 //    - the Infected makes its Normal neighbors Infected
 //    - the Infected become Immuned
 //    - the Immuned will stay Immuned
 // - only Infected people spreads virus
 var virusSpread = function (peopleCondition, hourInfected) {
-    var arr = peopleCondition.split('');
-    // let newCondition = '';
+    var arr = peopleCondition.split("");
     for (var i = 0; i < hourInfected; i++) {
+        var dup = arr.map(function (item) { return item; });
         for (var j = 0; j < arr.length; j++) {
-            if (arr[j] === 'o' && arr[j - 1] === '*' || arr[j + 1] === '*') {
-                arr[j] = '*';
+            if (dup[j] === "o" && dup[j - 1] === "*") {
+                arr[j] = "*";
             }
-            else if (arr[j] === '0') {
-                arr[j] = 'o';
+            else if (dup[j] === "o" && dup[j + 1] === "*") {
+                arr[j] = "*";
             }
-            else if (arr[j] === '*') {
-                arr[j] = '#';
+            else if (dup[j] === "o") {
+                arr[j] = "o";
             }
-            else if (arr[j] === '#') {
-                arr[j] = '#';
+            else if (dup[j] === "*") {
+                arr[j] = "#";
+            }
+            else if (dup[j] === "#") {
+                arr[j] = "#";
             }
         }
-        console.log(arr.join(''));
+        console.log(arr.join(""));
     }
-    return '---';
+    return "---";
 };
-console.log(virusSpread('*o##o', 1)); // #*##o
-console.log(virusSpread('o##o', 2)); // o##o
-console.log(virusSpread('oo*oo', 2)); // o*#*o -> *###*
-console.log(virusSpread('o#*o*o', 1)); // o##*#*
+console.log(virusSpread("*o##o", 2)); // #*##o -> ####o
+console.log(virusSpread("o#*#o", 1)); // o###o
+console.log(virusSpread("oo*oo", 3)); // o*#*o -> *###* => #####
+console.log(virusSpread("o#*o*o", 1)); // o##*#*
 //# sourceMappingURL=virus-spreads.js.map
--- Competitive-Programming/virus-spreads.ts
+++ Competitive-Programming/virus-spreads.ts
@@ -2,44 +2,45 @@
 // we want to simulate the spreads of the virus by its hours infected
 
 // notes:
-// Code          Condition
-// o             Normal
-// *             Infected
-// #             Immuned
+// Code     Condition
+// o        Normal
+// *        Infected
+// #        Immuned
 
 // rules:
 // - first line of input is the current conditions of the people
 // - second line of input is the hours of the virus spreads
 // - the output is the final condition of the people after hours they infected
-// - every next hours:  
+// - every next hours:
 //    - the Infected makes its Normal neighbors Infected
 //    - the Infected become Immuned
 //    - the Immuned will stay Immuned
 // - only Infected people spreads virus
 
 const virusSpread = (peopleCondition: string, hourInfected: number): string => {
-  const arr = peopleCondition.split('')
+  const arr = peopleCondition.split("");
 
-  // let newCondition = '';
-  for(let i = 0; i < hourInfected; i++){
-
-    for(let j = 0; j < arr.length; j++){
-      if(arr[j] === 'o' && arr[j - 1] === '*' || arr[j + 1] === '*'){
-        arr[j] = '*'
-      }else if (arr[j] === '0') {
-        arr[j] = 'o'
-      }else if (arr[j] === '*'){
-        arr[j] = '#'
-      }else if (arr[j] === '#'){
-        arr[j] = '#'
+  for (let i = 0; i < hourInfected; i++) {
+    const dup = arr.map((item) => item);
+    for (let j = 0; j < arr.length; j++) {
+      if (dup[j] === "o" && dup[j - 1] === "*") {
+        arr[j] = "*";
+      } else if (dup[j] === "o" && dup[j + 1] === "*") {
+        arr[j] = "*";
+      } else if (dup[j] === "o") {
+        arr[j] = "o";
+      } else if (dup[j] === "*") {
+        arr[j] = "#";
+      } else if (dup[j] === "#") {
+        arr[j] = "#";
       }
     }
-    console.log(arr.join('')) 
+    console.log(arr.join(""));
   }
-  return '---'
-}
+  return "---";
+};
 
-console.log(virusSpread('*o##o', 1)) // #*##o
-console.log(virusSpread('o##o', 2)) // o##o
-console.log(virusSpread('oo*oo', 2)) // o*#*o -> *###*
-console.log(virusSpread('o#*o*o', 1)) // o##*#*
+console.log(virusSpread("*o##o", 2)); // #*##o -> ####o
+console.log(virusSpread("o#*#o", 1)); // o###o
+console.log(virusSpread("oo*oo", 3)); // o*#*o -> *###* => #####
+console.log(virusSpread("o#*o*o", 1)); // o##*#*
