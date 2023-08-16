499c48b88fbdb89f6ff2b7b874a9ea867f87d4e7
Vincent <vincent14@student.umn.ac.id>
2023-08-16 18:03:27 +0700

    merge sorted array solved

--- Competitive-Programming/build/merge-sorted-array.js
+++ Competitive-Programming/build/merge-sorted-array.js
@@ -2,18 +2,15 @@
 // new arr 1 & 2 = number n & m
 // given 2 integer num1 and num2, m = items on num1 arr, n = items on num 2 arr
 // merge into single array sorted in non-decreasing order
-var merge = function (num1, m, num2, n) {
-    for (var i = 0; i < num1.length; i++) {
-        if (num1[i] === 0) {
-            console.log(i);
-            num1.splice(i, 1);
-        }
+// num1 has a length of m + n, where the m elements denote the elements that should be merge, and the last n elements are set to 0 and should be ignored. num2 has a length of n
+var merge = function (nums1, m, nums2, n) {
+    var index = nums1.length - n;
+    for (var i = index; i < nums1.length; i++) {
+        nums1[i] = nums2[i - index];
     }
-    console.log(num1);
-    return num2;
-    // return newN1.concat(newN2).sort();
+    return nums1.sort(function (a, b) { return a - b; });
 };
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
-// console.log(merge([1], 1, [], 0))
-// console.log(merge([0], 0, [1], 1))
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
 //# sourceMappingURL=merge-sorted-array.js.map
--- Competitive-Programming/merge-sorted-array.ts
+++ Competitive-Programming/merge-sorted-array.ts
@@ -2,19 +2,16 @@
 // new arr 1 & 2 = number n & m
 // given 2 integer num1 and num2, m = items on num1 arr, n = items on num 2 arr
 // merge into single array sorted in non-decreasing order
+// num1 has a length of m + n, where the m elements denote the elements that should be merge, and the last n elements are set to 0 and should be ignored. num2 has a length of n
 
-const merge = (num1: number[], m: number, num2: number[], n: number): number[] => {
-  for(let i = 0; i < num1.length; i++){
-    if(num1[i] === 0){
-      console.log(i)
-      num1.splice(i, 1);
-    }
+const merge = (nums1: number[], m: number, nums2: number[], n: number): number[] => {
+  const index = nums1.length - n;
+  for (let i = index; i < nums1.length; i++) {
+    nums1[i] = nums2[i - index];
   }
-  console.log(num1)
-  return num2
-  // return newN1.concat(newN2).sort();
+  return nums1.sort((a, b) => a - b);
 };
 
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
-// console.log(merge([1], 1, [], 0))
-// console.log(merge([0], 0, [1], 1))
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
