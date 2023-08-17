ae89358f2695340b833d81e8bb2a0fe9b50d532a
Vincent <vincent14@student.umn.ac.id>
2023-08-17 23:01:31 +0700

    point system solved

---
+++ Competitive-Programming/build/point-system-shooter-game.js
@@ -0,0 +1,12 @@
+// you are tasked for implementing point system for ur company upcoming shooter game
+// to encourage player spend more ammo, your company implement rule "point bracket"
+// Point Bracket
+// 1. bracket total kill 10 point multiplier x1
+// 2. bracket total kill 20 point multiplier x2
+// 3. bracket total kill 30 point multiplier x3
+// 4. bracket total kill 40 point multiplier x4
+// 5. bracket total kill 50 point multiplier x5
+// 5. bracket total kill above 50 point multiplier x6
+// if total kill <= 10, point = 10
+// if total kill >= 10 && <= 20, point = 2x
+//# sourceMappingURL=point-system-shooter-game.js.map
---
+++ Competitive-Programming/point-system-shooter-game.ts
@@ -0,0 +1,31 @@
+// you are tasked for implementing point system for ur company upcoming shooter game
+// to encourage player spend more ammo, your company implement rule "point bracket"
+
+// Point Bracket
+// 1. bracket total kill 10 point multiplier x1
+// 2. bracket total kill 20 point multiplier x2
+// 3. bracket total kill 30 point multiplier x3
+// 4. bracket total kill 40 point multiplier x4
+// 5. bracket total kill 50 point multiplier x5
+// 5. bracket total kill above 50 point multiplier x6
+
+const calculatePoint = (totalKill: number): number => {
+  if (totalKill <= 10) {
+    return 10;
+  }
+
+  let result = 0;
+  let multi = 1;
+  while (totalKill > 0) {
+    if (totalKill > 10) {
+      result += 10 * multi;
+    } else {
+      result += totalKill * multi;
+    }
+    totalKill -= 10;
+    if (totalKill < 6) {
+      multi++;
+    }
+  }
+  return result;
+};
